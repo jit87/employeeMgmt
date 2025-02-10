b1c5d84f769149fa2fc8f08c793f3962cca833ad
Ignacio (jit87) <ignaciotejada87@gmail.com>
2025-02-10 22:53:07 +0100

    Añadido html y ts de Asistencia

--- employeeMgmtClient/src/app/pages/contentpages/asistencia/asistencia.component.ts
+++ employeeMgmtClient/src/app/pages/contentpages/asistencia/asistencia.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { map, share, Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-asistencia',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrl: './asistencia.component.css'
 })
 export class AsistenciaComponent {
+
+  public horaActual: number; 
+
+  constructor() {
+    this.horaActual = 0; 
+  }
+
+
+  //Rejoj
+  time = new Date();
+  rxTime = new Date();
+  intervalId: any;
+  subscription: Subscription | undefined;
+
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        this.rxTime = time;
+      });
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
 
 }
